c4d7a5166e9c3bb9d12d7213125e57bc19b71ee4
aleremus <aleremusaleremus@gmail.com>
2022-06-24 15:21:52 +0300

    Implement `populate_command_list` method

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -404,8 +404,7 @@ void cg::renderer::dx12_renderer::copy_data(const void* buffer_data, UINT buffer
 
 	UINT8* buffer_data_begin;
 	CD3DX12_RANGE read_range(0, 0);
-	THROW_IF_FAILED(destination_resource->Map(0,
-											  &read_range, reinterpret_cast<void**>(&buffer_data_begin)));
+	THROW_IF_FAILED(destination_resource->Map(0,&read_range, reinterpret_cast<void**>(&buffer_data_begin)));
 
 	memcpy(buffer_data_begin, buffer_data, buffer_size);
 	destination_resource->Unmap(0,0);
@@ -524,7 +523,6 @@ void cg::renderer::dx12_renderer::load_assets()
 
 void cg::renderer::dx12_renderer::populate_command_list()
 {
-	// TODO Lab 3.06. Implement `populate_command_list` method
 	//RESET
 	THROW_IF_FAILED(command_allocators[frame_index]->Reset());
 	THROW_IF_FAILED(command_list->Reset(
@@ -537,6 +535,51 @@ void cg::renderer::dx12_renderer::populate_command_list()
 	command_list->SetDescriptorHeaps(_countof(heaps), heaps);
 	command_list->SetComputeRootDescriptorTable(0, cbv_srv_heap.get_gpu_descriptor_handle(0));
 	command_list->RSSetViewports(1,&view_port);
+	command_list->RSSetScissorRects(1, &scissor_rect);
+	command_list->ResourceBarrier(
+			1, &CD3DX12_RESOURCE_BARRIER::Transition(
+					   render_targets[frame_index].Get(),
+					   D3D12_RESOURCE_STATE_PRESENT,
+					   D3D12_RESOURCE_STATE_RENDER_TARGET));
+
+	//DRAWING
+
+	command_list->OMSetRenderTargets(
+			1,
+			&rtv_heap.get_cpu_descriptor_handle(frame_index),
+			FALSE,
+			nullptr);
+
+	const float clear_color[] = {0.f, 0.f, 0.f};
+
+	command_list->ClearRenderTargetView(
+			rtv_heap.get_cpu_descriptor_handle(frame_index),
+			clear_color,
+			0,
+			nullptr);
+
+	command_list->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	for (size_t s = 0; s < model->get_vertex_buffers().size(); s++)
+	{
+		command_list->IASetVertexBuffers(0,1,&vertex_buffer_views[s]);
+		command_list->IASetIndexBuffer(&index_buffer_views[s]);
+		command_list->DrawIndexedInstanced(
+				static_cast<UINT>(model->get_index_buffers()[s]->get_number_of_elements()),
+				1,
+				0,
+				0,
+				0);
+	}
+
+	command_list->ResourceBarrier(
+			1,
+			&CD3DX12_RESOURCE_BARRIER::Transition(
+					render_targets[frame_index].Get(),
+					D3D12_RESOURCE_STATE_RENDER_TARGET,
+					D3D12_RESOURCE_STATE_PRESENT));
+
+	THROW_IF_FAILED(command_list->Close());
 }
 
 
