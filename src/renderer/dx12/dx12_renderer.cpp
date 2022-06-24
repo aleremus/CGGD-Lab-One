628063ec1e3b9733f35e98bbe5ea20b6d61ad2c9
aleremus <aleremusaleremus@gmail.com>
2022-06-24 11:21:23 +0300

    Create a descriptor heap for a constant buffer

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -308,7 +308,12 @@ void cg::renderer::dx12_renderer::load_assets()
 			reinterpret_cast<void**>(&constant_buffer_data_begin)
 			));
 
-	// TODO Lab 3.04. Create a descriptor heap for a constant buffer
+	cbv_srv_heap.create_heap(
+			device,
+			D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+			1,
+			D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE
+			);
 	// TODO Lab 3.04. Create a constant buffer view
 }
 
